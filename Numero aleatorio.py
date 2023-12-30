fcf313de3afe141f6e5f985ac75b01e74add90b3
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2023-12-29 22:49:29 -0300

    Atualizar o Numero aleatorio.py

--- Numero aleatorio.py
+++ Numero aleatorio.py
@@ -1,1 +1,8 @@
-import random num_min = int(input("Digite o valor mínimo: ")) num_max = int(input("Digite o valor máximo: ")) numero_aleatorio = random.randint(num_min, num_max) print("Número aleatório:", numero_aleatorio)
+    import random
+
+num_min = int(input("Digite o valor mínimo: "))
+num_max = int(input("Digite o valor máximo: "))
+
+numero_aleatorio = random.randint(num_min, num_max)
+
+print("Número aleatório:", numero_aleatorio)
