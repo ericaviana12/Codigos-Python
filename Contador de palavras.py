4bda5ad0d2784e48ca8226c8f3f0d4bacc730a84
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2023-12-29 22:51:38 -0300

    Atualizar o Contador de palavras.py

--- Contador de palavras.py
+++ Contador de palavras.py
@@ -1,1 +1,6 @@
-texto = input("Digite um texto: ") palavras = texto.split() quantidade_palavras = len(palavras) print("O texto contém", quantidade_palavras, "palavras.")
+    texto = input("Digite um texto: ")
+
+palavras = texto.split()
+quantidade_palavras = len(palavras)
+
+print("O texto contém", quantidade_palavras, "palavras.")
